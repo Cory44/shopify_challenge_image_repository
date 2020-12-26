2616473fe08c99bdb31001c9b33168117b15830d
Cory Loughran <loughrancory@gmail.com>
2020-12-25 21:47:15 -0400

    Added a few user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'password_too_short' do
+    user = User.new(username: 'test-user', password: '12345', password_confirmation: '12345')
+    expect(user).to_not be_valid
+  end
+
+  it 'passwords_do_not_match' do
+    user = User.new(username: 'test-user', password: 'password', password_confirmation: 'wordpass')
+    expect(user).to_not be_valid
+  end
+
+  it 'username_is_nil' do
+    user = User.new(password: 'password', password_confirmation: 'password')
+    expect(user).to_not be_valid
+  end
+
+  it 'user_is_vald' do
+    user = User.new(username: 'test-user', password: 'password', password_confirmation: 'password')
+    expect(user).to be_valid
+  end
+
 end
